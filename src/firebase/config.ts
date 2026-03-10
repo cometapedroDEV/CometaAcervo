5df90c4233242ef61e57eec2190b9c9186bb4edd
Cometa Pedro <cometapedrojivago@gmail.com>
2026-03-10 08:39:31 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-2811492306-df10e",
+  "appId": "1:468106162238:web:9d923666e5d6ea0087e16f",
+  "apiKey": "AIzaSyDuIiVKrt1-8DJIlCB-ZJGZIo5WWx_vPoY",
+  "authDomain": "studio-2811492306-df10e.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "468106162238"
+};
